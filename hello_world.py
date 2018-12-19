b0288c61d722d64407224042217d719e45cd7a78
Dahandla <anykeyad@yahoo.com>
2018-12-19 07:33:29 -0500

    Create hello_world.py
    just a simple hello world in python

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+
+# Created by Andre' L. Dickinson, C:\code>Studio_
+
+
+# hello world program in python 
+
+print("Hello World")
+
+
+# End of Line
